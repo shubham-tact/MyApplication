e05f1346705298cea1fb389701adf2264a637cb6
shubham-tact <shubham.sh@tact.co.in>
2016-09-03 17:24:00 +0530

    Test 2

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -11,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("dhvfghd","iduhfoudhfo");
+
+
+        Log.d("dhvfghd","iduhfoudhfo");
+        Log.d("dhvfghd","iduhfoudhfo"); Log.d("dhvfghd","iduhfoudhfo"); Log.d("dhvfghd","iduhfoudhfo"); Log.d("dhvfghd","iduhfoudhfo");
+
+
+
     }
 }
